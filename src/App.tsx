dd1e845baa74555b2a897dfd1060c0768d0375c6
Christocentric1 <gomentifrank@gmail.com>
2025-12-11 23:33:57 +0000

    Change default route from /login to /dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App = () => (
         <BrowserRouter>
           <AuthProvider>
             <Routes>
-              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/login" element={<Login />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/alerts" element={<Alerts />} />
